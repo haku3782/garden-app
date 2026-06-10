3b7cad06a64cd7c7af3a16121024f92b9ab276b7
haku3782 <haku3782@gmail.com>
2026-06-10 16:24:02 +0900

    initial commit

---
+++ src/main/java/com/haku3782/garden_app/GardenAppApplication.java
@@ -0,0 +1,13 @@
+package com.haku3782.garden_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenAppApplication.class, args);
+	}
+
+}
